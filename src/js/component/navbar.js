f64c7cfe14b4b64b716f3583d44544a1bb87e2a0
Lucia Yebenes <yebeneslucia@gmail.com>
2021-05-10 20:01:32 +0000

    navbar y fetch

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starwars from "../../img/star-wars-white.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light mb-3 bg-transparent">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="starwarss navbar-brand mb-0 h1">
+					<img src={starwars} width="80" />
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary bg-transparent border-0">
+						<i className="fas fa-heart text-white" /> 0
+					</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,10 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import starwars from "../../img/star-wars.png";
 import "../../styles/home.scss";
+import Character from "../component/character.jsx";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+		<Character/>
 	</div>
 );
